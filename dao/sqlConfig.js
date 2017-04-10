6c38f187493b988063f990acd4d522f7ae0d72a8
林子 <1182874336@qq.com>
2017-04-10 11:48:57 +0800

    ts

--- dao/sqlConfig.js
+++ dao/sqlConfig.js
@@ -1,10 +1,17 @@
 
 module.exports = {
+//	mySql:{
+//		host:"127.0.0.1",
+//		user:"root",
+//		password:"root",
+//		database:"brush",
+//		port:"3306"
+//	},
 	mySql:{
-		host:"127.0.0.1",
-		user:"root",
-		password:"root",
-		database:"brush",
-		port:"3306"
+		host:"192.168.1.234",
+		user:"3acc1023b4504",
+		password:"fef8f8b6ce434",
+		database:"caf6039bde844",
+		port:"30061"
 	}
 };
