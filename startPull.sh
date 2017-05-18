cf2203555538ddf28c57167cf836c6edb62083e6
linzi <1182874336@qq.com>
2017-05-18 16:37:02 +0800

    ts

--- startPull.sh
+++ startPull.sh
@@ -3,7 +3,7 @@ WEB_PATH='/home/twobrush'
 WEB_USER='root'
 WEB_USERGROUP='root'
 echo "Start deployment"
-cd $WEB_PATH
+cd WEB_PATH
 echo "pulling source code..."
 git pull
 echo "changing permissions..."
