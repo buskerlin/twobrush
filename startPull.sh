5305c30c36e9ab5ef48da7d6289d6eeff63a2153
linzi <1182874336@qq.com>
2017-05-18 16:36:22 +0800

    ts

--- startPull.sh
+++ startPull.sh
@@ -5,9 +5,6 @@ WEB_USERGROUP='root'
 echo "Start deployment"
 cd $WEB_PATH
 echo "pulling source code..."
-git reset --hard origin/master
-git clean -f
 git pull
-git checkout master
 echo "changing permissions..."
 echo "Finished."
