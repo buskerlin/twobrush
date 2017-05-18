012f8d419879f50ec988eefd6692dc16dc3599d2
linzi <1182874336@qq.com>
2017-05-18 15:39:12 +0800

    ts

--- getPush.js
+++ getPush.js
@@ -27,5 +27,5 @@ handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref);
-  run_cmd('sh', ['./restart.sh'], function(text){ console.log(text) });
+  run_cmd('sh', ['./startPull.sh'], function(text){ console.log(text) });
 })
