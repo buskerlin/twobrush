ac4f0e984fe4af0ac377fcf4d2d5df3ed238a005
linzi <1182874336@qq.com>
2017-05-19 10:51:13 +0800

    change mysql config

--- dao/sqlConfig.js
+++ dao/sqlConfig.js
@@ -1,5 +1,7 @@
 
 module.exports = {
+	
+	//本地
 //	mySql:{
 //		host:"127.0.0.1",
 //		user:"root",
@@ -7,11 +9,22 @@ module.exports = {
 //		database:"brush",
 //		port:"3306"
 //	},
+
+	//魔泊云
+//	mySql:{
+//		host:"192.168.1.234",
+//		user:"3acc1023b4504",
+//		password:"fef8f8b6ce434",
+//		database:"caf6039bde844",
+//		port:"30061"
+//	},
+	
+	//阿里云
 	mySql:{
-		host:"192.168.1.234",
-		user:"3acc1023b4504",
-		password:"fef8f8b6ce434",
-		database:"caf6039bde844",
-		port:"30061"
+		host:"60.205.179.68",
+		user:"root",
+		password:"lql@1948",
+		database:"brush",
+		port:"3006"
 	}
 };
