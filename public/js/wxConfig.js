1ceafe40ccf7503f0346831f63a9795fa2061086
linzi <1182874336@qq.com>
2017-05-22 17:31:00 +0800

    ts

--- public/js/wxConfig.js
+++ public/js/wxConfig.js
@@ -1,15 +1,14 @@
-wx.config({
-    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    appId: '', // 必填，公众号的唯一标识
-    timestamp: '', // 必填，生成签名的时间戳
-    nonceStr: '', // 必填，生成签名的随机串
-    signature: '',// 必填，签名，见附录1
-    jsApiList: ['onMenuShareTimeline'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-});
 
-console.log(location.href);
 $.post("/wx/getSDKParams",{url:"http://www.bigwoods.cn"},function(res){
-	console.log(res);
+	res = res.data;
+	wx.config({
+	    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+	    appId: res.appId, // 必填，公众号的唯一标识
+	    timestamp: res.timestamp, // 必填，生成签名的时间戳
+	    nonceStr: res.noncestr, // 必填，生成签名的随机串
+	    signature: res.signature,// 必填，签名，见附录1
+	    jsApiList: ['onMenuShareTimeline'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+	});
 });
 
 //分享到朋友圈
@@ -26,5 +25,5 @@ wx.onMenuShareTimeline({
 });
 
 wx.ready(function(){
-	alert("success");
+	
 });
