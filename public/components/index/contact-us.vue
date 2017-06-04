e1f53d8935b7d703088e52b90551e366fca8c96f
linzi <1182874336@qq.com>
2017-06-04 15:06:45 +0800

    ts

--- public/components/index/contact-us.vue
+++ public/components/index/contact-us.vue
@@ -1,19 +1,21 @@
 <template>
 	<div class="contact-us">
+		<div>
+			<p class="tips">长按关注两把刷子官方微信号</p>
+			<div class="qrcode-box">
+				<span class="left"></span>
+				<span class="right"></span>
+				<hr></hr>
+				<img class="qrcode" src="/img/qrcode.png" />
+				<div>分享给好友</div>
+			</div>
+		</div>
 		<ul>
 			<li>
-				<p>拨打电话10086</p>
-				<span class="normal-button">点我拨打电话</span>
-				<p>或</p>
+				<a href="tel:15288181948"><span class="normal-button">拨打电话</span></a>
 			</li>
 			<li>
-				<p>关注官方公众号</p>
-				<img class="qrcode" src="/img/qrcode.png" />
-				<p>或</p>
-			</li>
-			<li>
-				<p>留下联系方式</p>
-				<input class="phone-input" type="text" id="" value="" />
+				<a href="sms:15288181948?body=你好，我是谁"><span class="normal-button">发送短信</span></a>
 			</li>
 		</ul>
 	</div>
@@ -54,10 +56,62 @@
 		top:0;
 		width:100%;
 		height:100%;
-		background-color:#fff;
+		background-color:#f5f5f5;
+		.tips {
+			color:@lightText;
+			font-size:0.6rem;
+			text-align: center;
+			margin:1.6rem 0 0.2rem 0;
+		}
+		.qrcode-box {
+			background-color:#fff;
+			width:11rem;
+			height:14rem;
+			margin:0 auto 1rem auto;
+			border-radius: 6px;
+			position:relative;
+			span {
+				position:absolute;
+				top:10rem;
+				width:0.5rem;
+				height:1rem;
+				background-color:#f5f5f5;
+			}
+			.left {
+				border-radius:0% 1rem 1rem 0;
+			}
+			.right {
+				border-radius:1rem 0 0 1rem;
+				right:0;
+			}
+			hr {
+				height:0;
+				border:1px dashed #eee;
+				-webkit-transform: scaleY(0.5);
+				width:80%;
+				vertical-align: middle;
+				display: inline-block;
+				position:absolute;
+				top:10.5rem;
+				margin:0 1rem;
+			}
+			.qrcode {
+				width:70%;
+				margin:0 auto;
+				padding-top:1.4rem;
+			}
+			div {
+				.button (@height:2rem,@radius:5px,@color:#00B7FF);
+				width:80%;
+				position:absolute;
+				bottom:0.8rem;
+				left:10%;
+				background-color:@red;
+			}
+		}
 		& > ul {
 			width: 60%;
-			margin:2rem auto;
+			margin:1rem auto;
 			& > li {
 				p:nth-child(1) {
 					color:@lightText;
@@ -75,6 +129,12 @@
 				.phone-input {
 					
 				}
+			}
+			.normal-button {
+				margin-bottom:0.5rem;
+				color:@blue;
+				background-color:#f5f5f5;
+				border:1px solid @blue;
 			}
 		}
 	}
